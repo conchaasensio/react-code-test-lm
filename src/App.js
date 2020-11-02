a21dbefbb1afbe11ca87564c4fd1f2310e7d4ed0
Concha Asensio <conchaasensiomr@gmail.com>
2020-11-02 17:43:52 +0100

    add api file and get data from api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import getDataFromApi from './server/api';
 import './App.css';
 
 function App() {
-  return <div className="App">Hola, mundo</div>;
+  const [sections, setSections] = useState([]);
+
+  useEffect(() => {
+    getDataFromApi();
+  }, []);
+
+  const renderData = () => {
+    const sectionsRendered = sections.map((section, index) => {
+      return (
+        <li className={index}>
+          <p>{section.name}</p>
+        </li>
+      );
+    });
+    return <ul>{sectionsRendered}</ul>;
+  };
+
+  return <div className="App">{renderData()}</div>;
 }
 
 export default App;
---
+++ src/server/api.js
@@ -0,0 +1,43 @@
+const getDataFromApi = async () => {
+  const token = await fetchUserToken();
+  const mercadonaId = await fetchMercadonaId(token);
+  return fetchCategories(token, mercadonaId);
+};
+
+export default getDataFromApi;
+
+function fetchUserToken() {
+  return fetch('https://api.comprea.com/v7/user/session')
+    .then((response) => response.json())
+    .then((sessionInformation) => {
+      console.log(sessionInformation);
+      return sessionInformation.token;
+    });
+}
+
+function fetchMercadonaId(token) {
+  const postalCode = '28010';
+  const url = `https://api.comprea.com/v7/user/postalcode?token=${token}&postalcode=${postalCode}`;
+  console.log(url);
+  return fetch(url)
+    .then((marketResponse) => marketResponse.json())
+    .then((marketResponseAsJson) => {
+      let markets = marketResponseAsJson.services.map(
+        (service) => service.markets
+      );
+      markets = [].concat.apply([], markets);
+      console.log(markets);
+      return markets.find((market) => market.name === 'Mercadona').id;
+    });
+}
+
+function fetchCategories(token, mercadonaId) {
+  return fetch(
+    `https://api.comprea.com/v7/company/categories?token=${token}&company_id=${mercadonaId}`
+  )
+    .then((categoriesResponse) => categoriesResponse.json())
+    .then((categories) => {
+      return categories.categories;
+    })
+    .then((categories) => console.log(categories));
+}
