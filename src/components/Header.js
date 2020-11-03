0877032ab4c5ee62ad1529c7adc4b2fc8771dfef
Concha Asensio <conchaasensiomr@gmail.com>
2020-11-03 17:04:52 +0100

    add first version header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,18 @@ import React from 'react';
 const Header = () => {
   return (
     <div className="header__container">
-      <img className="header__logo" src="" alt="" />
-      <h1 className="header__title">Mercadona</h1>
-      <span className="header__postalcode">Comprando en 28010</span>
-      <span className="header__option">Cambiar</span>
+      <div className="header__container-logo">
+        <img
+          className="header__logo"
+          src="../images/ic_circled_super.png"
+          alt="Logo Mercadona"
+        />
+      </div>
+      <div className="header__container-text">
+        <h1 className="header__title">Mercadona</h1>
+        <span className="header__postalcode">Comprando en 28010</span>
+      </div>
+      {/* <span className="header__option">Cambiar</span> */}
     </div>
   );
 };
